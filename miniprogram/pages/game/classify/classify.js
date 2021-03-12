af9b0e317e6f680359f2a4e7c06435930222e6b5
xjy825 <xujiangyun0825@163.com>
2021-03-12 18:13:09 +0800

    classify
    功能完成（排版和图片还没完成）

--- miniprogram/pages/game/classify/classify.js
+++ miniprogram/pages/game/classify/classify.js
@@ -1,18 +1,142 @@
 // miniprogram/pages/game/classify/classify.js
+const db = wx.cloud.database({
+  env: 'fit-gc46z'
+});  //用db代替数据库
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    //设备宽高
+    windowHeight:'',
+    windowWidth:'',
+    openid:'',
+    _id:'',
+    //游戏参数
+    buttonAnswer:'',
+    problemNumber:'',
+    imageSrc:'',
+    trueAnswer:'',
+    answer:-1,
+    score:0
+  },
 
+  getsize(){
+    let that=this;
+    wx.getSystemInfo({
+      success(res) {
+        that.setData({
+          windowHeight:res.windowHeight,
+          windowWidth:res.windowWidth
+        })
+      },
+    })
+  },
+
+  addScore(){
+    var that = this
+    if(that.data.buttonAnswer == that.data.trueAnswer){
+      this.setData({
+        answer:1
+      })
+      db.collection('userScore').doc(that.data._id)
+        .update({
+          data:{
+            score:this.data.score+1
+          }
+        })
+    }
+    else{
+      this.setData({
+        answer:0
+      })
+    }
+
+  },
+
+  button_low(){
+    this.setData({
+      buttonAnswer:'low'
+    })
+    this.addScore()
+    wx.navigateTo({
+      url: 'classify',
+    }) 
+  },
+
+  button_high(){
+    this.setData({
+      buttonAnswer:'high'
+    })
+    this.addScore()
+    wx.navigateTo({
+      url: 'classify',
+    }) 
+  },
+
+  getProblemPicture:function(){
+    //生成随机数
+    var Number;
+    Number = Math.floor(Math.random()+1).toString()
+    console.log(Number)
+    //从数据库中获取相应编号的题目
+    db.collection('classifyGameBank').where({
+      number:Number
+    }).get({
+      success: (res) => {
+        console.log(res.data[0].src)
+        this.setData({
+          imageSrc:res.data[0].src,
+          trueAnswer:res.data[0].highOrLow
+        })
+      },
+      fail: err =>{
+        console.log("错误")
+      }
+    })
+  },
+
+  getOpenid:function() {
+    let that = this;
+    wx.cloud.callFunction({ //调用getOpenid云函数
+      name: 'getOpenid',
+      data:{},
+      config:{env:"fit-gc46z"}
+    })
+    .then(res => { //调用getOpenid成功进行以下操作
+      console.log(res);
+      that.setData({
+        openid:res.result.openid
+      })
+      //从数据库中获取计分记录的id
+      db.collection('userScore').where({
+        openid:this.data.openid
+      }).get({
+        success: (res) => {
+          this.setData({
+            _id:res.data[0]._id,
+            score:res.data[0].score
+          })
+        },
+        fail: err =>{
+          console.log("错误")
+        }
+      })
+    })
+      .catch(err => { //调用getOpenid失败打印错误信息
+      console.log(err);
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getOpenid();//获取用户的openid
+    this.getsize();
+    this.getProblemPicture();
   },
 
   /**
