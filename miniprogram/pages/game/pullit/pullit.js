d23f1444b080d7c51c9371772e4ff3cb868dee4d
Xp <327746218@qq.com>
2021-03-25 17:05:43 +0800

    小游戏更新
    ...

--- miniprogram/pages/game/pullit/pullit.js
+++ miniprogram/pages/game/pullit/pullit.js
@@ -5,18 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-    // ishided1: "block",
-    // ishided2: "none",
     opacity1: 0,
     opacity2: 0,
-    top_distance: 0
+    top_distance: 0,
+    bottom_distance: 0,
+    pageHeight: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that=this;
+    //获得页面高度
+    wx.createSelectorQuery().select('.move_area').boundingClientRect(function(res){
+      that.setData({
+        pageHeight: res.bottom
+      });
+    }).exec()
   },
 
   /**
@@ -37,16 +43,31 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
   },
-  getlocation: function() {
+  onChange:function(){
+    this.getedgedistance();
+  },
+  //计算距离两个区域的距离
+  getedgedistance: function() {
     let that=this;
     wx.createSelectorQuery().select('#drag_item').boundingClientRect(function(res){
       that.setData({
-        top_distance: res.top
-        // opacity1: 0.5
+        top_distance: res.top,
+        bottom_distance: that.data.pageHeight-res.bottom
       });
+      that.ifgetin();
     }).exec()
-    console.log(this.data.top_distance);
+  },
+  //判断是否进入两个区域
+  ifgetin: function(){
+    let that=this;
+    that.setData({
+      opacity1: ((134.4-that.data.top_distance)/134.4).toFixed(2),
+      opacity2: ((134.4-that.data.bottom_distance)/134.4).toFixed(2)
+    })
+  },
+  //是否确认了
+  isconfirm: function(){
+    // console.log(12);
   }
 })
