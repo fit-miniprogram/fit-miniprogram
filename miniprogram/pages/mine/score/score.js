70537bc0a199575093aa120296d38a5bda771e6f
Xp <327746218@qq.com>
2021-04-02 11:28:08 +0800

    开场逻辑完成
    新用户进入开屏，老用户不进入

--- miniprogram/pages/mine/score/score.js
+++ miniprogram/pages/mine/score/score.js
@@ -1,66 +1,37 @@
-// miniprogram/pages/mine/score/score.js
-Page({
+const db = wx.cloud.database({
+  env: 'fit-gc46z'
+});  //用db代替数据库
 
+const app = getApp();
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-    tip: "已经到底啦*>﹏<*"
+    tip: "已经到底啦*>﹏<*",
+    rank_list: [],
+    //分数列表
+    rank_list_score: [],
+    //昵称和头像列表
+    rank_list_nickandprofile: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getUserList();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getUserList: function (){
+    let that=this;
+    db.collection('userScore')
+    .orderBy('score','desc')
+    .get()
+    .then(res=>{
+      console.log(res.data)
+      that.setData({
+        rank_list: res.data
+      })
+    })
   }
 })
